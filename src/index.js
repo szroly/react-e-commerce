b2d1970f5f314e77a1a183536fb511f74d162fa1
szroly <sep.roland@gmail.com>
2021-02-27 20:16:41 +0100

    update our menu-items

--- src/index.js
+++ src/index.js
@@ -4,7 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {BrowserRouter} from 'react-router-dom';
+
+ReactDOM.render(
+<BrowserRouter>
+    <App />
+</BrowserRouter>
+
+
+, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import Directory from '../../components/directory/directory.component';
 import './homepage.styles.scss';
 
-const HomePage = () => (
+const HomePage = (props) => (
+    
     <div className='homepage'>
+        
         <Directory />
     </div>
 );
